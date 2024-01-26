faa754c8a0d0a46d6e0b9ad621dfd92a8c7ef025
yuliia11111992 <YuliiaAntoniukja@gmail.com>
2024-01-26 13:57:06 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,37 +1,42 @@
+const form = document.querySelector('.feedback-form');
 
-const form=document.querySelector(".feedback-form");
-const setToLocalServer=()=>{
-   
-const email=form.elements.email.value;
-const message=form.elements.message.value;
-const feedbackForm={
-    email:email,
-    message:message
-}  
-localStorage.setItem("feedback-form-state",JSON.stringify(feedbackForm));
 
-    }
 
-const returnedForm=localStorage.getItem("feedback-form-state");
-if(returnedForm){
-  const parsedForm=JSON.parse(returnedForm);
-  form.elements.email.value=parsedForm.email;
-  form.elements.message.value=parsedForm.message;
+const saveToLocalStorage = () => {
+    const email = form.elements.email.value.trim();
+
+const message = form.elements.message.value.trim();
+
+const feedbackForm = {
+  email: email,
+  message: message,
+};
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackForm));
+};
+
+const returnedForm = localStorage.getItem('feedback-form-state');
+if (returnedForm) {
+  const parsedForm = JSON.parse(returnedForm);
+  form.elements.email.value = parsedForm.email;
+  form.elements.message.value = parsedForm.message;
 }
 
-const handleSubmit=(event)=>{
- event.preventDefault();
- console.log({
-    email:form.elements.email.value,
-    message:form.elements.message.value
-}  );
- localStorage.removeItem("feedback-form-state");
- form.reset();  
+const handleSubmit = event => {
+    const email = form.elements.email.value.trim();
+
+const message = form.elements.message.value.trim();
+
+const feedbackForm = {
+  email: email,
+  message: message,}
+  event.preventDefault();
+if (feedbackForm.email!=="" & feedbackForm.message!==""){
+console.log(feedbackForm);
+localStorage.removeItem('feedback-form-state');
+form.reset();
 }
 
+};
 
-
-
-
-form.addEventListener("input",setToLocalServer);
-form.addEventListener("submit",handleSubmit);
+form.addEventListener('input', saveToLocalStorage);
+form.addEventListener('submit', handleSubmit);
